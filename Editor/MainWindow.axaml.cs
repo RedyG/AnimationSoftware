72a68980663bd9e7be2f29a3c45fa884b5dbc883
RedyG <gagnonfrancois123@gmail.com>
2022-12-27 16:05:03 -0500

    Add project files.

---
+++ Editor/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Editor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
